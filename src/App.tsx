468b9e0f1fdffa3281ef7d1f27cbfc3358d0e7b8
Sabin Marcu <sabinmarcu@gmail.com>
2020-12-06 17:22:02 +0200

    added 404 page

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,10 @@ import { Container, withTheme } from '@material-ui/core';
 import styled from 'styled-components';
 
 import { use100vh } from 'react-div-100vh';
+
+import { baseURL } from './config/constants';
 import { Redirect } from './components/Redirect';
+
 import { NavBar } from './components/NavBar';
 import { Drawer, RouteChangeDrawerClose } from './components/Drawer';
 import CreateRecipe from './components/CreateRecipe';
@@ -13,7 +16,7 @@ import CreateRecipe from './components/CreateRecipe';
 import { HomeScreen } from './screens/Home';
 import { RecipeScreen, baseRoute as recipeRoute } from './screens/Recipe';
 import { TestScreen } from './screens/Test';
-import { baseURL } from './config/constants';
+import { Error404Screen } from './screens/404';
 
 const StyledContainerRaw = withTheme(
   styled(Container)`
@@ -43,6 +46,7 @@ export const App: FC = () => (
         <Route exact path="/" component={HomeScreen} />
         <Route path={recipeRoute} component={RecipeScreen} />
         <Route exact path="/test" component={TestScreen} />
+        <Route path="/" component={Error404Screen} />
       </Switch>
       <CreateRecipe />
     </StyledContainer>
