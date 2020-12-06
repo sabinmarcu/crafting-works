d7b19633984d8f0f9e22b450fa2c006c6e8ea457
Sabin Marcu <sabinmarcu@gmail.com>
2020-12-06 04:40:22 +0200

    add useMatchMedia and made theme react to prefered color scheme

--- src/hooks/useIsMobile.ts
+++ src/hooks/useIsMobile.ts
@@ -1,21 +1,6 @@
-import {
-  useState,
-  useEffect,
-} from 'react';
-
 import { mobileBreakpoint } from '../config/constants';
+import { useMatchMedia } from './useMatchMedia';
 
-const findIsMobile = () => window.matchMedia(mobileBreakpoint).matches;
-export const useIsMobile = () => {
-  const [isMobile, setIsMobile] = useState(findIsMobile());
-  useEffect(
-    () => {
-      const handler = () => setIsMobile(findIsMobile());
-      window.addEventListener('resize', handler);
-      return () => window.removeEventListener('resize', handler);
-    },
-  );
-  return isMobile;
-};
+export const useIsMobile = () => useMatchMedia(mobileBreakpoint);
 
 export default useIsMobile;
