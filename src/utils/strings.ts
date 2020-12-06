75bc4d0852cc1353ae0fc644a2f025cd2ae09a23
Sabin Marcu <sabinmarcu@gmail.com>
2020-12-07 00:22:14 +0200

    fixed naming in create recipe modal

--- src/utils/strings.ts
+++ src/utils/strings.ts
@@ -9,3 +9,8 @@ export const capitalizedToCamelCase = (
 ): string => `${input[0].toLowerCase()}${input
   .substr(1)
   .replace(/ [a-zA-Z]/g, (it) => it[1].toUpperCase())}`;
+
+export const capitalize = (
+  input: string,
+): string => input[0].toUpperCase()
+  + input.substr(1).replace(/ ([a-z])/g, (m) => ` ${m[1].toUpperCase()}`);
