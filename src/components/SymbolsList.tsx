634b88e5cfb7c06b90fc5009c5d3ea9d6a123c8d
Sabin Marcu <sabinmarcu@gmail.com>
2020-12-06 22:15:05 +0200

    filter symbols

--- src/components/SymbolsList.tsx
+++ src/components/SymbolsList.tsx
@@ -4,6 +4,7 @@ import styled from 'styled-components';
 import { useSymbols } from '../state/recipes-v3';
 import { StyledLink } from './styled';
 import { SymbolType } from '../utils/types';
+import { useFilter, Filter } from './Filter';
 
 export const StyledContainer = styled.div`
   display: flex;
@@ -22,29 +23,38 @@ export const ClickableStyledChip = styled(StyledChip)`
 
 export const SymbolsList: FC<{
   symbols: SymbolType[],
+  filter?: boolean,
   onDelete?: (input: string) => (e: Event) => void,
 }> = ({
   symbols,
   onDelete,
-}) => (
-  <StyledContainer>
-    {symbols.map(({ name, composite }) => (composite ? (
-      <StyledLink to={`/recipes/${name}`} key={name}>
-        <ClickableStyledChip
+  filter = true,
+}) => {
+  const { list, ...filterProps } = useFilter(
+    symbols,
+    ({ name }) => name,
+  );
+  return (
+    <StyledContainer>
+      {filter && <Filter {...filterProps} />}
+      {(filter ? list : symbols).map(({ name, composite }) => (composite ? (
+        <StyledLink to={`/recipes/${name}`} key={name}>
+          <ClickableStyledChip
+            label={name}
+            onDelete={onDelete && onDelete(name)}
+          />
+        </StyledLink>
+      ) : (
+        <StyledChip
+          variant="outlined"
           label={name}
+          key={name}
           onDelete={onDelete && onDelete(name)}
         />
-      </StyledLink>
-    ) : (
-      <StyledChip
-        variant="outlined"
-        label={name}
-        key={name}
-        onDelete={onDelete && onDelete(name)}
-      />
-    )))}
-  </StyledContainer>
-);
+      )))}
+    </StyledContainer>
+  );
+};
 
 export default () => {
   const symbols = useSymbols();
