4d4dac61bebc9723bc9821d2aff95c462041c0ba
Sabin Marcu <sabin.marcu@hootsuite.com>
2020-12-07 22:53:44 +0200

    fix: fix service worker builds

--- src/index.tsx
+++ src/index.tsx
@@ -9,7 +9,8 @@ import { TitleProvider } from './state/title';
 import { AppThemeProvider as ThemeProvider } from './state/theme';
 import { RecipeProviderV3 } from './state/recipes-v3';
 import { StackProvider } from './state/stack';
-import * as serviceWorker from './serviceWorker';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import reportWebVitals from './reportWebVitals';
 
 const rootElement = document.getElementById('root');
 render(
@@ -27,8 +28,16 @@ render(
   rootElement,
 );
 
-serviceWorker[
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration[
   process.env.NODE_ENV === 'production'
     ? 'register'
     : 'unregister'
 ]();
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals(console.log);
