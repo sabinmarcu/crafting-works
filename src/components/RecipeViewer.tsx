06d0b86901487035c1213120a9d5c2ac6b6746d5
Sabin Marcu <sabinmarcu@gmail.com>
2020-12-07 03:29:01 +0200

    added stack view to recipe viewer

--- src/components/RecipeViewer.tsx
+++ src/components/RecipeViewer.tsx
@@ -14,6 +14,8 @@ import {
   AccordionSummary,
   AccordionDetails,
   Tab,
+  withTheme,
+  Theme,
 } from '@material-ui/core';
 
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
@@ -39,6 +41,7 @@ import { camelCaseToCapitalized } from '../utils/strings';
 import { Title } from '../state/title';
 import { useIsMobile } from '../hooks/useIsMobile';
 import { Visualization } from './Visualization';
+import { useStacks } from '../state/stack';
 
 export const StyledContainer = styled(Container)`
   display: grid !important;
@@ -50,19 +53,81 @@ export const StyledContainer = styled(Container)`
   }
 `;
 
-export const ResourceWrapper = styled.div`
-  display: flex;
-  flex-flow: row nowrap;
-  align-items: center;
-  justify-content: space-between;
-`;
+const backgroundGetter = ({
+  theme:
+  {
+    palette: {
+      background: {
+        default: val,
+      },
+    },
+  },
+}: { theme: Theme }) => val;
+const borderRadiusGetter = ({
+  theme:
+  {
+    shape: {
+      borderRadius: val,
+    },
+  },
+}: { theme: Theme }) => val;
+const borderGetter = ({
+  theme:
+  {
+    palette: {
+      background: {
+        paper: val,
+      },
+    },
+  },
+}: { theme: Theme }) => val;
+
+export const ResourceWrapper = withTheme(
+  styled.div<{
+    columns?: number,
+    bold?: boolean,
+  }>`
+    display: grid;
+    grid-template-columns: 5fr repeat(${({ columns }) => columns || 1}, 100px);
+    background: ${backgroundGetter};
+    overflow: hidden;
+    &:first-of-type {
+      border-top-left-radius: ${borderRadiusGetter}px;
+      border-top-right-radius: ${borderRadiusGetter}px;
+    }
+    & * {
+      font-weight: ${({ bold }) => (bold ? 'bold' : 'normal')} !important;
+      font-size: ${({ bold }) => (bold ? '1.2' : '1')}rem !important;
+    }
+    & > * {
+      padding: 10px;
+      border-top: solid 1px ${borderGetter};
+      border-left: solid 1px ${borderGetter};
+      &:last-child {
+        border-right: solid 1px ${borderGetter};
+      }
+    }
+    &:last-of-type {
+      border-bottom-left-radius: ${borderRadiusGetter}px;
+      border-bottom-right-radius: ${borderRadiusGetter}px;
+      & > * {
+        border-bottom: solid 1px ${borderGetter};
+      }
+    }
+  `,
+);
 
 export const ResourceText = styled(Typography)`
   flex: 1;
 `;
 
 export const ResourceNumber = styled(Typography)`
-
+  font-feature-settings: tnum;
+  font-variant-numeric: tabular-nums;
+  display: flex;
+  flex-flow: row nowrap;
+  align-items: center;
+  justify-content: flex-start;
 `;
 
 export const SVGCard = styled(Card)`
@@ -70,18 +135,49 @@ export const SVGCard = styled(Card)`
   flex-flow: column nowrap;
 `;
 
+export const ResourceHeader: FC<{
+  title: string
+}> = ({
+  title,
+}) => {
+  const { isEnabled } = useStacks();
+  return (
+    <ResourceWrapper columns={isEnabled ? 2 : 1} bold>
+      <ResourceText>{title}</ResourceText>
+      <ResourceNumber>
+        {isEnabled ? 'Stacks' : 'Amount'}
+      </ResourceNumber>
+      {isEnabled && (
+        <ResourceNumber>Amount</ResourceNumber>
+      )}
+    </ResourceWrapper>
+  );
+};
+
 export const ResourcePreview: FC<{
   text: string,
   amount: number
 }> = ({
   text,
   amount,
-}) => (
-  <ResourceWrapper>
-    <ResourceText>{camelCaseToCapitalized(text)}</ResourceText>
-    <ResourceNumber>{amount}</ResourceNumber>
-  </ResourceWrapper>
-);
+}) => {
+  const { isEnabled, stackSize } = useStacks();
+  return (
+    <ResourceWrapper columns={isEnabled ? 2 : 1}>
+      <ResourceText>{camelCaseToCapitalized(text)}</ResourceText>
+      <ResourceNumber>
+        {isEnabled && stackSize
+          ? Math.ceil(amount / stackSize!)
+          : amount}
+      </ResourceNumber>
+      {isEnabled && stackSize && (
+      <ResourceNumber>
+        {`(${amount})`}
+      </ResourceNumber>
+      )}
+    </ResourceWrapper>
+  );
+};
 
 const ResourcesSummary: FC = () => {
   const resources = useResources();
@@ -94,6 +190,7 @@ const ResourcesSummary: FC = () => {
     <Card>
       <CardHeader title="Total Resouces" />
       <CardContent>
+        <ResourceHeader title="Resource" />
         {rootResources.map(
           ([key, value]) => (
             <ResourcePreview key={key} text={key} amount={value} />
@@ -186,6 +283,7 @@ export const StepsView: FC = () => {
             {`Step ${idx + 1}`}
           </AccordionSummary>
           <StyledAccordionDetails>
+            <ResourceHeader title="Recipes" />
             {step.map(
               ({ name, amount }) => (
                 <ResourcePreview key={name} text={name} amount={amount} />
